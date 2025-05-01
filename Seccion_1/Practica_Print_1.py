dc4dc1b46c24be96ba7274dc19040e97e42694be
Lordsaturos <raul.rivero.engineer@gmail.com>
2025-05-01 18:06:00 +0200

    Cambiado el directorio a al raiz del curso para que contenga toda la información

---
+++ Seccion_1/Practica_Print_1.py
@@ -0,0 +1,221 @@
+# Comentario. Curso de Udemy. Declaraciones de print.
+# Contenido texto Practica 1
+
+print("Hello world!"); # Escribimos por pantalla "Hello world". Si queremos intercambiar las comillas,
+# Podemos intercambiar las comillas simples por dobles o viceversa.
+print("200");
+print('Nuevo ejemplo');
+print("Ejemplo 'intercambio' de comillas");
+print("Ejemplo2 'Intercambio' de comillas");
+print('Ejemplo3 "intercambio" de comillas');
+print("Ejemplo4 \"intercambio\" de comillas"); # Para escribir comillas dobles dentro de comillas dobles, se escapan con \.
+# Ejercicio de codificación 1: Práctica Print 2
+
+# ************************************************
+# Ejercicio de codificación 1: Práctica Print 1
+# Práctica Print 1
+    # Crea un código que imprima en pantalla la expresión Me encanta estudiar Python.
+    # Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+    # ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+
+print("Me encanta estudiar Python"); # Escribimos por pantalla "Me encanta estudiar Python".
+
+#End
+
+# ************************************************
+# Ejercicio de codificación 2: Práctica Print 2
+# Práctica Print 2
+    # Crea un código que imprima en pantalla la expresión Estudiar con "Python Total" es super divertido
+    # Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+    # ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+
+print("Estudiar con \"Python Total\" es super divertido."); # Escribimos por pantalla "Estudiar con "Python Total" es super divertido".)
+
+
+#End
+
+# ************************************************
+# Ejercicio de codificación 3: Práctica Print 3
+# Práctica Print 3
+    # Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
+    # Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+    # ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+print(500+55); # Escribimos por pantalla "555".
+
+#End
+
+# Estudio de Strings
+# Strings Los strings en Python son un tipo de dato formado por cadenas (o secuencias) de caracteres de cualquier tipo, 
+# formando un texto. concatenación Unificación de cadenas de texto: print("Hola" + " " + "mundo") >> Hola mundo caracteres 
+# especiales Indicamos a la consola que el caracter a continuación del símbolo \ debe ser tratado como un caracter especial.
+# \" > Imprime comillas \n > Separa texto en una nueva linea \t > Imprime un tabulador \\ > Imprime la barra invertida 
+# textualmentePython TOTAL
+
+print('100 + 40 + 30'); # Escribimos por pantalla "100 + 40 + 30".
+
+print("Hola" + 'Fede'); # Escribimos por pantalla "HolaFede".
+print("Hola" + " " + 'Fede'); # Escribimos por pantalla "Hola Fede".
+print("Hola" + " " + 'Fede' + " " + "¿cómo estás?"); # Escribimos por pantalla "Hola Fede ¿cómo estás?".
+
+print("Hola, me llamo \'Raúl\'"); # Escribimos por pantalla "Hola, me llamo 'Raúl'".
+
+print("Esta es una linea\nY esta es otra linea"); # Escribimos por pantalla "Esta es una linea" y "Y esta es otra linea" en dos lineas diferentes.
+print("\tEsta es la tercera linea") # Escribimos por pantalla "Esta es la tercera linea" con un tabulador al principio.
+
+print('This isn\'t a number'); # Escribimos por pantalla "This isn't a number".
+
+print("Este signo \\ es una barra invertida"); # Escribimos por pantalla "Este signo \ es una barra invertida".
+
+# ************************************************
+# Ejercicio de codificación 4: Práctica String 1
+# Práctica String 1
+    # Crea un código que imprima en pantalla la siguiente expresión (pero usando print una sola vez):
+
+    #    Línea 1
+    #    Línea 2
+    #    Línea 3
+    # Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+    # ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+print("Línea 1\nLínea 2\nLínea 3"); # Escribimos por pantalla "Línea 1", "Línea 2" y "Línea 3" en tres lineas diferentes.
+
+#End
+
+# ************************************************
+# Ejercicio de codificación 5: Práctica String 2
+# Práctica String 2
+    # Crea un código que imprima en pantalla la siguiente expresión.
+        #    A	B	C
+        #    D	E	F
+        #    G	H	I
+    # Notas:
+    # - Observa que la distancia horizontal entre los números ha sido creada con UNA tabulación.
+    # - Puedes usar una o más declaraciones print
+    # Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+    # ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+print("A\tB\tC");
+print("D\tE\tF");
+print("G\tH\tI");
+
+#End
+
+# ************************************************
+# Ejercicio de codificación 6: Práctica String 3
+# Práctica String 3
+    # Crea un código que imprima en pantalla la siguiente expresión:
+    # Barra Normal: /
+    # Barra Invertida: \
+    # Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+# ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+print("Barra Normal: /");
+print("Barra Invertida: \\");
+
+#End
+
+# Estudio de Input
+# input Función que permite al usuario introducir información por medio del teclado al ejecutarse, otorgándole una 
+# instrucción acerca del ingreso solicitado. El código continuará ejecutándose luego de que el usuario realice la acción. 
+# input("Dime tu nombre: ") >> Dime tu nombre: | Ingreso por teclado print("Tu nombre es " + input("Dime tu nombre: ")) 
+# >> Dime tu nombre: Federico >> Tu nombre es Federico Python TOTAL
+
+print(input("Dime tu nombre: "));
+
+print("Tu nombre es: " + input("Dime tu Nombre: ") + " " + input("Dime tu apellido: "));
+
+
+
+# ************************************************
+# Ejercicio de codificación 7: Práctica Input 1
+# Práctica Input 1
+    # Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+    # ¿Qué estás estudiando?
+    # El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+    # Aclaración:
+        # 1. No deben imprimirse strings adicionales a la respuesta del usuario.
+        # 2. Los ejercicios que contienen el la función input() deben ser probados con el botón: "Ejecutar pruebas".  Ya que el 
+        # botón "Ejecutar código" arrojará el siguiente error: "EOF when reading a line".
+    # ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada 
+    # sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar 
+    # diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así 
+    # tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio 
+    # de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+print(input("¿Qué estás estudiando? "));
+
+#End
+
+# ************************************************
+# Ejercicio de codificación 8: Práctica Input 2
+# Práctica Input 2
+# Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+# ¿En qué país vives?
+# El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+# Aclaración:
+    # 1. no deben imprimirse strings adicionales a la respuesta del usuario.
+    # 2. Los ejercicios que contienen el la función input() deben ser probados con el botón: "Ejecutar pruebas".  Ya que el botón "Ejecutar código" arrojará el siguiente error: "EOF when reading a line".
+# ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+print(input("¿En qué país vives? "));
+
+#End
+
+# ************************************************
+# Ejercicio de codificación 9: Práctica Input 3
+# Práctica Input 3
+# Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con 
+# las siguientes instrucciones:
+    # Escribe tu nombre:
+    # Escribe tu apellido:
+# El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+# Aclaración:
+    # 1. No deben imprimirse strings adicionales a la respuesta del usuario.
+    # 2. Los ejercicios que contienen el la función input() deben ser probados con el botón: "Ejecutar pruebas".  Ya que el 
+    # botón "Ejecutar código" arrojará el siguiente error: "EOF when reading a line".
+# ¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección 
+# posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias 
+# rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a 
+# resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y 
+# Respuestas que tienes a tu disposición. Muchos éxitos!
+# ************************************************
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "));
+
+#End
+
+
+# ************************************************
+# Proyecto del Día 1
+# Llegó el momento más esperado de tu primer día de estudio. Vas a crear tu primer programa
+# solo y lo vas a hacer aplicando todo lo que has aprendido a lo largo de esta jornada. Imagina
+# esta situación: tu mejor amigo ha puesto una fábrica de cerveza y tiene todo listo. Su producto
+# es fantástico, tiene cuerpo, buen sabor, buen color, el nivel justo de espuma… pero le falta una
+# identidad. No se le ocurre qué nombre ponerle su cerveza para que tenga una identidad única
+# y original. Entonces vienes tú y le dices "No te preocupes, yo voy a crear un programa que te
+# va a hacer dos preguntas y luego te va a decir cuál es el nombre de tu cerveza". Así de simple.
+# Ya sé que en el mundo real no necesitaríamos desarrollar un software solo para hacer dos
+# preguntas, pero hasta que aprendamos más funcionalidades los programas van a tener que
+# mantenerse en el terreno de lo simple. Igualmente, si está recién comenzando, este va a ser
+# todo un desafío.
+
+# Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que
+# requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras
+# combinadas, para formar una marca creativa.
+# Puedes usar las preguntas que quieras. La idea es que el resultado sea original, creativo, y hasta
+# cómico, y si quieres agregar dificultad al desafío, te sugiero que intentes que el nombre de la
+# cerveza se imprima entre comillas. Recuerda que hay diferentes formas de que la función print
+# muestre las comillas sin cortar el string, y que ingrese la impresión final en al menos dos líneas
+# utilizando saltos de línea dentro del código.
+
+# Intenta hacerlo por tu cuenta y si se complica, no te preocupes, en la próxima elección lo vamos
+# a resolver juntos. 
+# ************************************************
+
+#Escribir nombre de la Cerveza
+
+print("\"" + input("Nombre Animal: ") + " " + input("Nombre Adjetivo: ") + "\"");
+
+#End
